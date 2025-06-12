844f355ab79920ca1b1f9152ef4fecfd6b7986c1
jotaccback <jotaccback@gmail.com>
2025-06-12 11:53:34 -0300

    Corrige erro de importação de Session.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import SessionCreate from "./SessionCreate";
+import SessionJoin from "./SessionJoin";
 
-import App from './App';
-import SessionCreate from './SessionCreate';
-import SessionJoin from './SessionJoin';
-
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
